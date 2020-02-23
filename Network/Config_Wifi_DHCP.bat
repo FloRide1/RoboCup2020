25aa3386c1ca324fe521e91fab7c19c3c85b4453
Capucine <vgies@hotmail.com>
2020-02-23 15:41:44 +0100

    Update Config_Wifi_DHCP.bat

--- Network/Config_Wifi_DHCP.bat
+++ Network/Config_Wifi_DHCP.bat
@@ -2,7 +2,6 @@
 ECHO Resetting IP Address and Subnet Mask For DHCP
 netsh interface set interface "Wi-Fi" admin=enable
 netsh int ip set address name = "Wi-Fi" source = dhcp
-netsh interface set interface "Ethernet" admin=disable
 ipconfig /renew
 ECHO Here are the new settings for %computername%:
 netsh int ip show config
