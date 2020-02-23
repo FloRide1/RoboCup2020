85f0aef1ec1c80c49cae549ff20a483bfbc75266
TABLE2 <TABLE2>
2020-02-23 16:17:49 +0100

    update config reseau

--- Network/Config_Robot4.bat
+++ Network/Config_Robot4.bat
@@ -1,6 +1,6 @@
 echo "Setting Network Configuration for Robot 1"
 netsh interface set interface "Wi-Fi" admin=enable
 netsh interface set interface "Ethernet" admin=disable
-netsh interface ip set address "Wi-Fi" static 172.16.79.104 255.255.255.0 172.16.1.1 1
+netsh interface ip set address "Wi-Fi" static 172.16.79.104 255.255.255.0 172.16.0.1 1
 netsh int ip show config
 pause
