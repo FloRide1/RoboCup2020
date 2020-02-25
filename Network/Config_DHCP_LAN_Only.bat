ca8a3a0f52d7240c44dfdb80e9105588895ed3e6
iutgeiitoulon <vgies@univ-tln.fr>
2020-02-25 23:32:40 +0100

    update config réseau DHCP

--- Network/Config_DHCP_LAN_Only.bat
+++ Network/Config_DHCP_LAN_Only.bat
@@ -3,7 +3,10 @@ ECHO Resetting IP Address and Subnet Mask For DHCP
 netsh interface set interface "Wi-Fi" admin=disable
 netsh interface set interface "Ethernet" admin=enable
 netsh int ip set address name = "Ethernet" source = dhcp
+netsh interface ip set dnsservers name="Ethernet" source=dhcp
+ipconfig /release
 ipconfig /renew
+ipconfig /registerdns
 ECHO Here are the new settings for %computername%:
 netsh int ip show config
 
--- Network/Config_DHCP_Wifi_LAN.bat
+++ Network/Config_DHCP_Wifi_LAN.bat
@@ -2,9 +2,13 @@
 ECHO Resetting IP Address and Subnet Mask For DHCP
 netsh interface set interface "Wi-Fi" admin=enable
 netsh int ip set address name = "Wi-Fi" source = dhcp
+netsh interface ip set dnsservers name="Wi-Fi" source=dhcp
 netsh interface set interface "Ethernet" admin=enable
 netsh int ip set address name = "Ethernet" source = dhcp
+netsh interface ip set dnsservers name="Ethernet" source=dhcp
+ipconfig /release
 ipconfig /renew
+ipconfig /registerdns
 ECHO Here are the new settings for %computername%:
 netsh int ip show config
 
