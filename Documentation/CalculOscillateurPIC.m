f3f98486cf2da0a02957c6b679d56627e9a396a3
Valentin Gies <gies@univ-tln.fr>
2020-11-17 00:56:14 +0100

    minor change

--- Documentation/CalculOscillateurPIC.m
+++ Documentation/CalculOscillateurPIC.m
@@ -1,10 +1,13 @@
 %Calcul de la fréquence du dsPIC33EP512MU814 sur Oscillateur Fast RC en
 %mode FRCPLL (Internal Fast RC with PLL)
+%Contraintes : F_IN/(PLL_PRE+2) entre 0.8 MHz et 8 MHz
+%Contraintes : F_VCO = F_IN*(PLL_DIV+2)/(PLL_PRE+2) entre 120 MHz et 340 MHz
+%Contraintes : F_OSC entre 15 MHz et 140 MHz à 85°
 
-Fin = 7.37;
+Fin = 32; %7.37;
 PLLPOST = 0;
-PLLPRE = 0;
-PLLDIV = 63;
+PLLPRE = 4;
+PLLDIV = 43;
 
 FPLLI = Fin/(PLLPRE+2);
 FVCO = FPLLI*(PLLDIV+2);
