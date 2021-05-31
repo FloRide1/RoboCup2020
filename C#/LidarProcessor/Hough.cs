8ab3bede739b83843de3af37e3c7cc0e0a1a76d8
FloRide <florian.reimat@orange.fr>
2021-05-31 18:52:19 +0200

    Add a bit Hough

--- C#/LidarProcessor/Hough.cs
+++ C#/LidarProcessor/Hough.cs
@@ -36,15 +36,27 @@ namespace LidarProcessor
         {
             Vector<double>[][] array_of_vectorize_cluster = new Vector<double>[0][];
 
-
+            /// DNFORGET
             foreach (Vector<double>[] cluster in array_of_vectorize_cluster)
             {
                 int numbers_of_pixels_in_cluster = cluster.Length;
 
                 Vector<double> mean_point = Vector<double>.Build.DenseOfArray(new double[2] { cluster.Sum(x => x[0]) / numbers_of_pixels_in_cluster, cluster.Sum(x => x[1]) / numbers_of_pixels_in_cluster });
-                //(cluster - mean_point)
 
+                double Sxx = 0.0, Sxy = 0.0, Syy = 0.0, Syx = 0.0;
 
+                foreach (Vector<double> vector in cluster)
+                {
+                    double x = vector[0] - mean_point[0];
+                    double y = vector[1] - mean_point[1];
+
+                    Sxx += Math.Pow(x, 2);
+                    Syy += Math.Pow(y, 2);
+                    Sxy += x * y;
+                }
+                Syx = Sxy;
+                
+                Matrix<double> matrix = Matrix.Cons
             }
 
         }
